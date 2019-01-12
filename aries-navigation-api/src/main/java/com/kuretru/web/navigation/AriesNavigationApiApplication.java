a6987d591f7736011fbebfe0cb30b5c7842ce8ae
呉真 <kuretru@gmail.com>
2019-01-12 21:43:17 +0800

    新增：项目结构

---
+++ aries-navigation-api/src/main/java/com/kuretru/web/navigation/AriesNavigationApiApplication.java
@@ -0,0 +1,14 @@
+package com.kuretru.web.navigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriesNavigationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AriesNavigationApiApplication.class, args);
+    }
+
+}
+
