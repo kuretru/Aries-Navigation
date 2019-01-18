bd7dcb5c0417790868b0900bc735d2e52c7cc45a
呉真 <kuretru@gmail.com>
2019-01-18 21:21:12 +0800

    新增：引入Dozer框架

--- aries-navigation-api/src/main/java/com/kuretru/web/navigation/Application.java
+++ aries-navigation-api/src/main/java/com/kuretru/web/navigation/Application.java
@@ -2,11 +2,13 @@ package com.kuretru.web.navigation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author kuretru
  */
 @SpringBootApplication
+@ComponentScan({"com.kuretru.web.navigation", "com.kuretru.api.common"})
 public class Application {
 
     public static void main(String[] args) {
