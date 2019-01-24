3ed293a468097ec2e79034326a4bc22fc238d326
呉真 <kuretru@gmail.com>
2019-01-24 15:56:51 +0800

    新增：分类、站点Mapper

---
+++ aries-navigation-api/src/main/java/com/kuretru/web/navigation/mapper/WebCategoryMapper.java
@@ -0,0 +1,11 @@
+package com.kuretru.web.navigation.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kuretru.web.navigation.entity.data.WebCategoryDO;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+public interface WebCategoryMapper extends BaseMapper<WebCategoryDO> {
+
+}
---
+++ aries-navigation-api/src/main/java/com/kuretru/web/navigation/mapper/WebSiteMapper.java
@@ -0,0 +1,10 @@
+package com.kuretru.web.navigation.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+public interface WebSiteMapper extends BaseMapper<WebSiteMapper> {
+
+}
