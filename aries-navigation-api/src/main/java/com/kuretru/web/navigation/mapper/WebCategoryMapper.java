2f2ebdcf841d3aff7d627ae4d2b9bc08382f6244
呉真 <kuretru@gmail.com>
2019-01-25 17:49:47 +0800

    修复：DO实体继承错误的问题

--- aries-navigation-api/src/main/java/com/kuretru/web/navigation/mapper/WebCategoryMapper.java
+++ aries-navigation-api/src/main/java/com/kuretru/web/navigation/mapper/WebCategoryMapper.java
@@ -2,10 +2,14 @@ package com.kuretru.web.navigation.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kuretru.web.navigation.entity.data.WebCategoryDO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
  */
+@Mapper
+@Repository
 public interface WebCategoryMapper extends BaseMapper<WebCategoryDO> {
 
 }
--- aries-navigation-api/src/main/java/com/kuretru/web/navigation/mapper/WebSiteMapper.java
+++ aries-navigation-api/src/main/java/com/kuretru/web/navigation/mapper/WebSiteMapper.java
@@ -1,10 +1,15 @@
 package com.kuretru.web.navigation.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kuretru.web.navigation.entity.data.WebSiteDO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
  */
-public interface WebSiteMapper extends BaseMapper<WebSiteMapper> {
+@Mapper
+@Repository
+public interface WebSiteMapper extends BaseMapper<WebSiteDO> {
 
 }
