e882a56cd3c495f0ca14a6c5268c87446cab420d
呉真 <kuretru@gmail.com>
2019-01-27 11:11:49 +0800

    新增：基本查询功能

--- aries-navigation-api/src/main/java/com/kuretru/web/navigation/controller/WebTagController.java
+++ aries-navigation-api/src/main/java/com/kuretru/web/navigation/controller/WebTagController.java
@@ -2,10 +2,12 @@ package com.kuretru.web.navigation.controller;
 
 import com.kuretru.api.common.controller.BaseRestController;
 import com.kuretru.api.common.entity.ApiResponse;
+import com.kuretru.web.navigation.service.WebCategoryService;
 import com.kuretru.web.navigation.service.WebTagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/tags", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class WebTagController extends BaseRestController {
-    @Autowired
+
     private WebTagService service;
+
+    private WebCategoryService categoryService;
+
+    @Autowired
+    public WebTagController(WebTagService service, WebCategoryService categoryService) {
+        this.service = service;
+        this.categoryService = categoryService;
+    }
 
     @GetMapping()
     public ApiResponse list() {
         return ApiResponse.success(service.list());
+    }
+
+    @GetMapping("/{tag_id}/categories")
+    public ApiResponse listCategories(@PathVariable("tag_id") long tagId) {
+        return ApiResponse.success(categoryService.listByTagId(tagId));
     }
 
 }
--- aries-navigation-api/src/main/java/com/kuretru/web/navigation/entity/data/WebCategoryDO.java
+++ aries-navigation-api/src/main/java/com/kuretru/web/navigation/entity/data/WebCategoryDO.java
@@ -10,7 +10,7 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-@TableName("web_cateogry")
+@TableName("web_category")
 public class WebCategoryDO extends BaseDO {
 
     private Integer tagId;
