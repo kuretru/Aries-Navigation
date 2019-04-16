7477557f02eb1f5e9e66a113085e0a3f8c3e6812
呉真 <kuretru@gmail.com>
2019-04-16 09:37:54 +0800

    新增：项目配置文件

--- Aries-Navigation-Api/src/main/java/com/kuretru/web/navigation/Application.java
+++ Aries-Navigation-Api/src/main/java/com/kuretru/web/navigation/Application.java
@@ -2,8 +2,10 @@ package com.kuretru.web.navigation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.kuretru.web.navigation", "com.kuretru.api.common"})
 public class Application {
 
     public static void main(String[] args) {
