3936de1f2d3db34d13a57118a2d81e64e019d110
呉真 <kuretru@gmail.com>
2019-04-16 09:24:14 +0800

    新增：标签实体类

---
+++ Aries-Navigation-Api/src/main/java/com/kuretru/web/navigation/entity/data/WebSiteDO.java
@@ -0,0 +1,26 @@
+package com.kuretru.web.navigation.entity.data;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.kuretru.api.common.entity.data.BaseDO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("web_site")
+public class WebSiteDO extends BaseDO {
+
+    private Long categoryId;
+
+    private String name;
+
+    private String imageUrl;
+
+    private String siteUrl;
+
+    private Integer sequence;
+
+}
