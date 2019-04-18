5d1d7441e8a63e79e86e392fdb9e881b7b761d95
呉真 <kuretru@gmail.com>
2019-04-18 11:10:37 +0800

    新增：为首页导航站数据创建缓存功能

--- Aries-Navigation-Api/src/main/java/com/kuretru/web/navigation/Application.java
+++ Aries-Navigation-Api/src/main/java/com/kuretru/web/navigation/Application.java
@@ -2,10 +2,17 @@ package com.kuretru.web.navigation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
 @SpringBootApplication
 @ComponentScan({"com.kuretru.web.navigation", "com.kuretru.api.common"})
+@EnableAsync
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {
--- Aries-Navigation-Api/src/main/java/com/kuretru/web/navigation/entity/view/WebSiteVO.java
+++ Aries-Navigation-Api/src/main/java/com/kuretru/web/navigation/entity/view/WebSiteVO.java
@@ -3,11 +3,13 @@ package com.kuretru.web.navigation.entity.view;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
  */
 @Data
-public class WebSiteVO {
+public class WebSiteVO implements Serializable {
 
     @JsonIgnore
     private Long id;
