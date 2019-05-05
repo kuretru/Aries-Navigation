dd1a3281ba278b4eac3c5473d992ce386a2b08de
呉真 <kuretru@gmail.com>
2019-05-05 21:25:35 +0800

    修改：单元测试载入的配置文件

--- Aries-Navigation-Api/aries-navigation-api/src/test/java/com/kuretru/web/navigation/ApplicationTests.java
+++ Aries-Navigation-Api/aries-navigation-api/src/test/java/com/kuretru/web/navigation/ApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles(profiles = "development")
+@ActiveProfiles("development")
 public class ApplicationTests {
 
     @Test
