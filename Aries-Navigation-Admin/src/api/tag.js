6d796ba2b7f80db2cec865a4f9e1185f95815f00
呉真 <kuretru@gmail.com>
2019-05-15 21:16:35 +0800

    新增：标签管理CRUD操作

--- Aries-Navigation-Admin/src/api/tag.js
+++ Aries-Navigation-Admin/src/api/tag.js
@@ -7,3 +7,26 @@ export function list(params) {
     params
   })
 }
+
+export function create(data) {
+  return request({
+    url: '/tags/',
+    method: 'post',
+    data
+  })
+}
+
+export function update(data) {
+  return request({
+    url: '/tags/' + data.id,
+    method: 'put',
+    data
+  })
+}
+
+export function remove(id) {
+  return request({
+    url: '/tags/' + id,
+    method: 'delete'
+  })
+}
