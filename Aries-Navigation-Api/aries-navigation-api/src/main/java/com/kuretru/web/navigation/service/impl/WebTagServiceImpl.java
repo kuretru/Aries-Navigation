f6ca084e492f66f4ed49f2b95eeffe52f3e958f3
呉真 <kuretru@gmail.com>
2019-05-16 10:30:57 +0800

    新增：对实体重新排序的接口

--- Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/impl/WebTagServiceImpl.java
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/impl/WebTagServiceImpl.java
@@ -1,5 +1,7 @@
 package com.kuretru.web.navigation.service.impl;
 
+import com.kuretru.api.common.exception.ApiException;
+import com.kuretru.api.common.exception.NotFoundException;
 import com.kuretru.api.common.service.impl.BaseServiceImpl;
 import com.kuretru.web.navigation.entity.data.WebTagDO;
 import com.kuretru.web.navigation.entity.transfer.WebTagDTO;
@@ -7,6 +9,9 @@ import com.kuretru.web.navigation.mapper.WebTagMapper;
 import com.kuretru.web.navigation.service.WebTagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
@@ -32,6 +37,23 @@ public class WebTagServiceImpl extends BaseServiceImpl<WebTagMapper, WebTagDO, W
     public int getMaxSequence() {
         Integer result = mapper.getMaxSequence();
         return result == null ? 0 : result;
+    }
+
+    @Override
+    public int reorder(List<Long> idList) throws ApiException {
+        List<WebTagDO> records = new ArrayList<>(idList.size());
+        int sequence = 1;
+        for (Long id : idList) {
+            WebTagDO record = new WebTagDO();
+            record.setId(id);
+            record.setSequence(sequence++);
+            records.add(record);
+        }
+        Integer result = mapper.updateBatchByIds(records);
+        if (result == null || result != idList.size()) {
+            throw new NotFoundException("部分记录不存在！");
+        }
+        return result;
     }
 
 }
