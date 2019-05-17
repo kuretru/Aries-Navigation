3d7a82e839c6a8c75c4d9711de9302e51be26468
呉真 <kuretru@gmail.com>
2019-05-17 20:36:20 +0800

    新增：根据TagID查Category的服务

--- Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/WebCategoryService.java
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/WebCategoryService.java
@@ -5,9 +5,19 @@ import com.kuretru.web.navigation.entity.data.WebCategoryDO;
 import com.kuretru.web.navigation.entity.transfer.WebCategoryDTO;
 import com.kuretru.web.navigation.mapper.WebCategoryMapper;
 
+import java.util.List;
+
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
  */
 public interface WebCategoryService extends BaseService<WebCategoryMapper, WebCategoryDO, WebCategoryDTO> {
+
+    /**
+     * 获取指定TagID下的所有记录
+     *
+     * @param tagId TagID
+     * @return 指定TagID下的所有记录DTO
+     */
+    List<WebCategoryDTO> list(long tagId);
 
 }
