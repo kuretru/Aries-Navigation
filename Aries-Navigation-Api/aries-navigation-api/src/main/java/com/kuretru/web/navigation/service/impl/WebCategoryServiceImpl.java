04cf4d72cdc2fee1ccd72bdf3e115020e73890ab
呉真 <kuretru@gmail.com>
2019-05-17 20:36:20 +0800

    新增：根据TagID查Category的服务

--- Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/impl/WebCategoryServiceImpl.java
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/impl/WebCategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kuretru.web.navigation.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kuretru.api.common.service.impl.BaseServiceImpl;
 import com.kuretru.web.navigation.entity.data.WebCategoryDO;
 import com.kuretru.web.navigation.entity.transfer.WebCategoryDTO;
@@ -7,6 +8,8 @@ import com.kuretru.web.navigation.mapper.WebCategoryMapper;
 import com.kuretru.web.navigation.service.WebCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
@@ -17,6 +20,15 @@ public class WebCategoryServiceImpl extends BaseServiceImpl<WebCategoryMapper, W
     @Autowired
     public WebCategoryServiceImpl(WebCategoryMapper mapper) {
         super(mapper, WebCategoryDO.class, WebCategoryDTO.class);
+    }
+
+    @Override
+    public List<WebCategoryDTO> list(long tagId) {
+        QueryWrapper<WebCategoryDO> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("tag_id", tagId);
+        queryWrapper.orderByAsc("sequence");
+        List<WebCategoryDO> records = mapper.selectList(queryWrapper);
+        return doToDTO(records);
     }
 
 }
