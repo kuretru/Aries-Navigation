92af13b5a3dd5b717bc3ac1f36fd9675ec2efccf
呉真 <kuretru@gmail.com>
2019-05-18 23:18:44 +0800

    新增：分类管理界面，标签组件

---
+++ Aries-Navigation-Admin/src/api/category.js
@@ -0,0 +1,42 @@
+import request from '@/utils/request'
+
+export function list(params) {
+  return request({
+    url: '/tags/',
+    method: 'get',
+    params
+  })
+}
+
+export function create(data) {
+  return request({
+    url: '/tags/',
+    method: 'post',
+    data
+  })
+}
+
+export function update(data) {
+  return request({
+    url: '/tags/' + data.id,
+    method: 'put',
+    data
+  })
+}
+
+export function remove(id) {
+  return request({
+    url: '/tags/' + id,
+    method: 'delete'
+  })
+}
+
+export function reorder(ids) {
+  return request({
+    url: '/tags/reorder',
+    method: 'put',
+    data: {
+      idList: ids
+    }
+  })
+}
