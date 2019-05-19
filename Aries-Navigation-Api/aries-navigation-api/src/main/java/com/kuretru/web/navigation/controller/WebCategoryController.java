cfca4e62e2ff2ba0b706e40b5eed7a791efb06ca
呉真 <kuretru@gmail.com>
2019-05-19 12:05:02 +0800

    新增：分类管理基本CRUD接口

--- Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/controller/WebCategoryController.java
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/controller/WebCategoryController.java
@@ -1,18 +1,16 @@
 package com.kuretru.web.navigation.controller;
 
 import com.kuretru.api.common.annotation.RequestAuthorization;
-import com.kuretru.api.common.controller.BaseController;
+import com.kuretru.api.common.controller.BaseCrudController;
 import com.kuretru.api.common.entity.ApiResponse;
 import com.kuretru.api.common.exception.ApiException;
 import com.kuretru.api.common.exception.NotFoundException;
 import com.kuretru.web.navigation.entity.transfer.WebCategoryDTO;
 import com.kuretru.web.navigation.service.WebCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,23 +19,43 @@ import java.util.List;
  */
 @RestController
 @RequestMapping(value = "/api/tags/{tagId}/categories", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-public class WebCategoryController extends BaseController {
-
-    private WebCategoryService service;
+public class WebCategoryController extends BaseCrudController<WebCategoryService, WebCategoryDTO> {
 
     @Autowired
     public WebCategoryController(WebCategoryService service) {
-        this.service = service;
+        super(service);
     }
 
     @RequestAuthorization
     @GetMapping()
-    public ApiResponse listByTagId(@PathVariable("tagId") Long tagId) throws ApiException {
+    public ApiResponse list(@PathVariable("tagId") Long tagId) throws ApiException {
         List<WebCategoryDTO> result = service.list(tagId);
         if (result.isEmpty()) {
             throw new NotFoundException("未找到相关对象！");
         }
         return ApiResponse.success(result);
+    }
+
+    @RequestAuthorization
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse create(@PathVariable("tagId") long tagId, @RequestBody WebCategoryDTO record) throws ApiException {
+        record.setTagId(tagId);
+        return super.create(record);
+    }
+
+    @RequestAuthorization
+    @PutMapping("/{id}")
+    public ApiResponse update(@PathVariable("tagId") Long tagId, @PathVariable("id") Long id, @RequestBody WebCategoryDTO record) throws ApiException {
+        record.setTagId(tagId);
+        return super.update(id, record);
+    }
+
+    @RequestAuthorization
+    @DeleteMapping("/{id}")
+    @Override
+    public ApiResponse remove(@PathVariable("id") Long id) throws ApiException {
+        return super.remove(id);
     }
 
 }
