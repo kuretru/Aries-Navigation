81cf1af04836f5b2cb0c35ec48781c527f293326
呉真 <kuretru@gmail.com>
2019-05-22 15:37:26 +0800

    新增：列出所有站点接口

--- Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/impl/WebSiteServiceImpl.java
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/impl/WebSiteServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kuretru.web.navigation.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kuretru.api.common.service.impl.BaseServiceImpl;
 import com.kuretru.web.navigation.entity.data.WebSiteDO;
 import com.kuretru.web.navigation.entity.transfer.WebSiteDTO;
@@ -7,6 +8,8 @@ import com.kuretru.web.navigation.mapper.WebSiteMapper;
 import com.kuretru.web.navigation.service.WebSiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
@@ -17,6 +20,15 @@ public class WebSiteServiceImpl extends BaseServiceImpl<WebSiteMapper, WebSiteDO
     @Autowired
     public WebSiteServiceImpl(WebSiteMapper mapper) {
         super(mapper, WebSiteDO.class, WebSiteDTO.class);
+    }
+
+    @Override
+    public List<WebSiteDTO> list(long categoryId) {
+        QueryWrapper<WebSiteDO> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("category_id", categoryId);
+        queryWrapper.orderByAsc("sequence");
+        List<WebSiteDO> records = mapper.selectList(queryWrapper);
+        return doToDTO(records);
     }
 
 }
