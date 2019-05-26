9a9c6228aafbe61740a2efd3d9563c9973ee2995
呉真 <kuretru@gmail.com>
2019-05-26 10:55:42 +0800

    修改：支持浏览器调试

--- Aries-Navigation-Admin/vue.config.js
+++ Aries-Navigation-Admin/vue.config.js
@@ -54,7 +54,8 @@ module.exports = {
       alias: {
         '@': resolve('src')
       }
-    }
+    },
+    devtool: 'source-map'
   },
   chainWebpack(config) {
     config.plugins.delete('preload') // TODO: need test
