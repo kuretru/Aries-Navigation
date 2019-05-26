f5c4b7b9c58cd8fff3da9e4df23b6c3190f7e104
呉真 <kuretru@gmail.com>
2019-05-26 11:19:05 +0800

    新增：导航管理页面

--- Aries-Navigation-Admin/src/api/site.js
+++ Aries-Navigation-Admin/src/api/site.js
@@ -9,31 +9,35 @@ export function list(tagId, categoryId, params) {
 }
 
 export function create(data) {
+  delete data.tagList
+  delete data.categoryList
   return request({
-    url: '/tags/' + data.tagId + '/categories/',
+    url: '/tags/' + data.tagId + '/categories/' + data.categoryId + '/sites/',
     method: 'post',
     data
   })
 }
 
 export function update(data) {
+  delete data.tagList
+  delete data.categoryList
   return request({
-    url: '/tags/' + data.tagId + '/categories/' + data.id,
+    url: '/tags/' + data.tagId + '/categories/' + data.categoryId + '/sites/' + data.id,
     method: 'put',
     data
   })
 }
 
-export function remove(tagId, id) {
+export function remove(tagId, categoryId, id) {
   return request({
-    url: '/tags/' + tagId + '/categories/' + id,
+    url: '/tags/' + tagId + '/categories/' + categoryId + '/sites/' + id,
     method: 'delete'
   })
 }
 
-export function reorder(tagId, ids) {
+export function reorder(tagId, categoryId, ids) {
   return request({
-    url: '/tags/' + tagId + '/categories/reorder',
+    url: '/tags/' + tagId + '/categories/' + categoryId + '/sites/reorder',
     method: 'put',
     data: {
       idList: ids
