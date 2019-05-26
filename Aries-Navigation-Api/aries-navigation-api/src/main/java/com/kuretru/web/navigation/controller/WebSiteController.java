81ccf7204d29c9fffaf0297fd5a17d0468b807c3
呉真 <kuretru@gmail.com>
2019-05-26 10:16:26 +0800

    新增：站点CRUD接口

--- Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/controller/WebSiteController.java
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/controller/WebSiteController.java
@@ -9,6 +9,7 @@ import com.kuretru.web.navigation.entity.transfer.WebFaviconDTO;
 import com.kuretru.web.navigation.entity.transfer.WebSiteDTO;
 import com.kuretru.web.navigation.service.WebSiteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +35,28 @@ public class WebSiteController extends BaseCrudController<WebSiteService, WebSit
             throw new NotFoundException("未找到相关对象！");
         }
         return ApiResponse.success(result);
+    }
+
+    @RequestAuthorization
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse create(@PathVariable("categoryId") long categoryId, @RequestBody WebSiteDTO record) throws ApiException {
+        record.setCategoryId(categoryId);
+        return super.create(record);
+    }
+
+    @RequestAuthorization
+    @PutMapping("/{id}")
+    public ApiResponse update(@PathVariable("categoryId") Long categoryId, @PathVariable("id") Long id, @RequestBody WebSiteDTO record) throws ApiException {
+        record.setCategoryId(categoryId);
+        return super.update(id, record);
+    }
+
+    @RequestAuthorization
+    @DeleteMapping("/{id}")
+    @Override
+    public ApiResponse remove(@PathVariable("id") Long id) throws ApiException {
+        return super.remove(id);
     }
 
     @RequestAuthorization
