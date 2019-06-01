2eb1e11a3369aebe57b5e476ba30e6fd85249d89
呉真 <kuretru@gmail.com>
2019-06-01 19:58:41 +0800

    修改：axios封装

--- Aries-Navigation-Admin/src/settings.js
+++ Aries-Navigation-Admin/src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: '白羊导航 - 后台管理',
 
   /**
    * @type {boolean} true | false
--- Aries-Navigation-Admin/src/utils/request.js
+++ Aries-Navigation-Admin/src/utils/request.js
@@ -3,64 +3,46 @@ import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
-// create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  baseURL: process.env.VUE_APP_BASE_API,
+  withCredentials: true,
+  timeout: 5000
 })
 
-// request interceptor
 service.interceptors.request.use(
   config => {
-    // do something before request is sent
-
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
       config.headers['Access-Token'] = getToken()
     }
     return config
   },
   error => {
-    // do something with request error
-    console.log(error) // for debug
+    console.log(error)
     return Promise.reject(error)
   }
 )
 
-// response interceptor
 service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-  */
-
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
   response => {
     const res = response.data
-
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code >= 2999) {
+    if (res.code >= 4000) {
       Message({
-        message: res.data || 'error',
+        message: res.data || 'Error',
         type: 'error',
         duration: 5 * 1000
       })
-
-      return Promise.reject(res.message || 'error')
+      return Promise.reject(res.message || 'Error')
     } else {
       return res
     }
   },
   error => {
-    console.log('err' + error) // for debug
     const res = error.response.data
+    if (res.code) {
+      console.log(res.code + '-' + res.message + '：' + res.data)
+    } else {
+      console.log(res.status + '-' + res.error + '：' + res.message)
+    }
     Message({
       message: res.data,
       type: 'error',
@@ -68,7 +50,6 @@ service.interceptors.response.use(
     })
 
     if (res.code === 4011) {
-      // to re-login
       MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
         confirmButtonText: 'Re-Login',
         cancelButtonText: 'Cancel',
@@ -79,7 +60,6 @@ service.interceptors.response.use(
         })
       })
     }
-
     return Promise.reject(error)
   }
 )
