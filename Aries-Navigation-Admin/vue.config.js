007af15d734251f41cc595fd96e3b9237c94f405
呉真 <kuretru@gmail.com>
2019-06-02 12:26:56 +0800

    升级：vue-cli版本至3.8.0，修复：运行项目时打开2次浏览器的问题

--- Aries-Navigation-Admin/vue.config.js
+++ Aries-Navigation-Admin/vue.config.js
@@ -7,20 +7,10 @@ function resolve(dir) {
 }
 
 const name = defaultSettings.title || 'vue Admin Template' // page title
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-const port = 7011 // dev port
 
-// All configuration item explanations can be find in https://cli.vuejs.org/config/
+const port = 7011
+
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
   publicPath: '/admin/',
   outputDir: 'dist',
   assetsDir: 'static',
@@ -28,15 +18,13 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     port: port,
-    open: true,
+    open: false,
     overlay: {
       warnings: false,
       errors: true
     }
   },
   configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
     name: name,
     resolve: {
       alias: {
