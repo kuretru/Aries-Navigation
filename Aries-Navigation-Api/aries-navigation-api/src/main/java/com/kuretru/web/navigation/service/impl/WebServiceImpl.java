7ec513fd58c3abf045390a5619c20106a31b4958
呉真 <kuretru@gmail.com>
2019-07-04 11:06:00 +0800

    修复：构造导航数据时，未拼接CDN地址的错误

--- Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/impl/WebServiceImpl.java
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/navigation/service/impl/WebServiceImpl.java
@@ -1,6 +1,10 @@
 package com.kuretru.web.navigation.service.impl;
 
+import com.kuretru.api.common.configuration.CommonProperties;
+import com.kuretru.api.common.util.StringUtils;
 import com.kuretru.web.navigation.configuration.SystemConstants;
+import com.kuretru.web.navigation.entity.view.WebCategoryVO;
+import com.kuretru.web.navigation.entity.view.WebSiteVO;
 import com.kuretru.web.navigation.entity.view.WebTagVO;
 import com.kuretru.web.navigation.mapper.WebMapper;
 import com.kuretru.web.navigation.service.WebService;
@@ -20,17 +24,29 @@ public class WebServiceImpl implements WebService {
 
     private static final String CACHE_KEY = "ARIES" + SystemConstants.INDEX_DATA_CACHE_KEY;
 
-    private WebMapper mapper;
+    private final WebMapper mapper;
+    private final CommonProperties commonProperties;
 
     @Autowired
-    public WebServiceImpl(WebMapper mapper) {
+    public WebServiceImpl(WebMapper mapper, CommonProperties commonProperties) {
         this.mapper = mapper;
+        this.commonProperties = commonProperties;
     }
 
     @Cacheable(value = CACHE_KEY, key = "#root.targetClass", condition = "#useCache != false")
     @Override
     public List<WebTagVO> list(boolean useCache) {
-        return mapper.listTags();
+        List<WebTagVO> result = mapper.listTags();
+        for (WebTagVO webTagVO : result) {
+            List<WebCategoryVO> webCategories = webTagVO.getCategories();
+            for (WebCategoryVO webCategoryVO : webCategories) {
+                List<WebSiteVO> webSites = webCategoryVO.getSites();
+                for (WebSiteVO webSiteVO : webSites) {
+                    fixCdnUrl(webSiteVO);
+                }
+            }
+        }
+        return result;
     }
 
     @CachePut(value = CACHE_KEY, key = "#root.targetClass")
@@ -43,6 +59,13 @@ public class WebServiceImpl implements WebService {
     @Override
     public void removeCache() {
 
+    }
+
+    private void fixCdnUrl(WebSiteVO record) {
+        if (!StringUtils.isNullOrEmpty(record.getImageUrl())) {
+            String imageUrl = commonProperties.getFileCdnPrefix() + record.getImageUrl() + commonProperties.getFileCdnSuffix();
+            record.setImageUrl(imageUrl);
+        }
     }
 
 }
