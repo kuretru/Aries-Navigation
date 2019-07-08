8ffca92803047fadf5db0caed8c14ff8aedcece8
呉真 <kuretru@gmail.com>
2019-07-08 16:57:48 +0800

    新增：页面基本布局

--- Aries-Navigation-Web/src/api/data.js
+++ Aries-Navigation-Web/src/api/data.js
@@ -2,7 +2,7 @@ import request from '../utils/request'
 
 export function list() {
   return request({
-    url: 'http://127.0.0.1:7010/api/data',
+    url: 'http://127.0.0.1:7010/api/data?use_cache=false',
     method: 'get'
   })
 }
