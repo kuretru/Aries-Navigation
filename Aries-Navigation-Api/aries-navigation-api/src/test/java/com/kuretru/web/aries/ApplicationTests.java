f35fce7b9e36ebc0ff58185c64313b0102e5b873
呉真 <kuretru@gmail.com>
2021-03-03 20:18:46 +0800

    构建(API)：新增项目目录结构

---
+++ Aries-Navigation-Api/aries-navigation-api/src/test/java/com/kuretru/web/aries/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kuretru.web.aries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
