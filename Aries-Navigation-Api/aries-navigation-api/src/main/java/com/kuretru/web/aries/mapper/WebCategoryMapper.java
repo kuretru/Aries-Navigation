bc5677165b8bde58dcd60e83c7f419512dca045b
呉真 <kuretru@gmail.com>
2021-03-05 10:35:41 +0800

    新增(API)：核心数据访问层

---
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/aries/mapper/WebCategoryMapper.java
@@ -0,0 +1,15 @@
+package com.kuretru.web.aries.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kuretru.web.aries.entity.WebCategoryDO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@Mapper
+@Repository
+public interface WebCategoryMapper extends BaseMapper<WebCategoryDO> {
+
+}
---
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/aries/mapper/WebTagMapper.java
@@ -0,0 +1,15 @@
+package com.kuretru.web.aries.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kuretru.web.aries.entity.WebTagDO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@Mapper
+@Repository
+public interface WebTagMapper extends BaseMapper<WebTagDO> {
+
+}
