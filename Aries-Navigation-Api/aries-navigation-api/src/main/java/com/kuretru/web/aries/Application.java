dd8ab794f92e7c432391a39b00dee68f4a6a4386
呉真 <kuretru@gmail.com>
2022-04-19 21:09:07 +0800

    删除(API)：不适用Spring Data Redis

--- Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/aries/Application.java
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/aries/Application.java
@@ -4,11 +4,12 @@ import com.kuretru.microservices.oauth2.client.system.EnableOAuth2System;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisAutoConfiguration.class)
 @EnableOAuth2System
 @MapperScan("com.kuretru.web.aries.mapper")
 public class Application {
