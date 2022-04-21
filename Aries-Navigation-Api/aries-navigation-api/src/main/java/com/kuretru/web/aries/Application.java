6a9f44b2b8afe12f33f0f93033d674685d9f7280
呉真 <kuretru@gmail.com>
2022-04-21 15:27:51 +0800

    优化(API)：优雅的移除Redis依赖

--- Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/aries/Application.java
+++ Aries-Navigation-Api/aries-navigation-api/src/main/java/com/kuretru/web/aries/Application.java
@@ -4,12 +4,11 @@ import com.kuretru.microservices.oauth2.client.system.EnableOAuth2System;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
-@SpringBootApplication(exclude = RedisAutoConfiguration.class)
+@SpringBootApplication
 @EnableOAuth2System
 @MapperScan("com.kuretru.web.aries.mapper")
 public class Application {
