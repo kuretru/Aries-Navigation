255291fb318d1b830f37bdcb96529f3653a55f94
呉真 <kuretru@gmail.com>
2023-02-17 10:50:16 +0800

    修复(Web)：二级分类布局错位的问题

--- Aries-Navigation-Web/src/pages/Index/WebCategory.tsx
+++ Aries-Navigation-Web/src/pages/Index/WebCategory.tsx
@@ -9,15 +9,19 @@ interface WebCategoryProps {
 const WebCategoryView: React.FC<WebCategoryProps> = ({ category }) => {
   return (
     <ProCard bordered split="vertical" key={category.id}>
-      <div className={styles.category_head}>{category.name}</div>
-      <ProCard className={styles.category_body} colSpan={24} wrap>
-        {category.sites.map((item) => {
-          return (
-            <ProCard colSpan={4} key={item.id} size="small">
-              <WebSiteView site={item} />
-            </ProCard>
-          );
-        })}
+      <ProCard bodyStyle={{ padding: 5 }} className={styles.category_name} colSpan={"100px"} layout='center' >
+        {category.name}
+      </ProCard>
+      <ProCard bodyStyle={{ padding: 0 }} colSpan={"auto"}>
+        <ProCard className={styles.category_body} colSpan={24} wrap>
+          {category.sites.map((item) => {
+            return (
+              <ProCard colSpan={4} key={item.id} size="small">
+                <WebSiteView site={item} />
+              </ProCard>
+            );
+          })}
+        </ProCard>
       </ProCard>
     </ProCard>
   );
