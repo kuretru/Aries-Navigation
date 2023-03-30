e2283851a27b8c9a5c258f6f32ca779cb847fd50
呉真 <kuretru@gmail.com>
2023-03-30 13:48:40 +0800

    新增(Web)：显示隐藏标签按钮

--- Aries-Navigation-Web/src/pages/Index/WebIndex.tsx
+++ Aries-Navigation-Web/src/pages/Index/WebIndex.tsx
@@ -6,10 +6,12 @@ import type { ItemType, MenuInfo } from 'rc-menu/lib/interface';
 import { getRecords } from '@/services/aries-navigation';
 import WebTagView from './WebTag';
 import styles from './index.less';
+import { SwapOutlined } from '@ant-design/icons';
 interface IWebIndexState {
   tags: API.Web.WebTagVO[];
   menuItems: ItemType[];
   currentIndex: number;
+  showHiddenTag: boolean;
 }
 class WebIndex extends React.Component<Record<string, never>, IWebIndexState> {
   constructor(props: Record<string, never>) {
@@ -18,21 +20,33 @@ class WebIndex extends React.Component<Record<string, never>, IWebIndexState> {
       tags: [],
       menuItems: [],
       currentIndex: 0,
+      showHiddenTag: false,
     };
-    this.fetchData();
+    this.fetchData().then(() => this.renderMenu(this.state.showHiddenTag));
   }
 
-  fetchData = () => {
-    getRecords().then((response) => {
-      const menuItems: ItemType[] = [];
-      response.data.forEach((tag: API.Web.WebTagVO) =>
-        menuItems.push({ key: tag.id, label: tag.name }),
-      );
-      this.setState({ tags: response.data, menuItems: menuItems });
+  fetchData = async () => {
+    await getRecords().then((response) => {
+      this.setState({ tags: response.data });
     });
   };
 
+  renderMenu = (showHiddenTag: boolean) => {
+    const menuItems: ItemType[] = [];
+    this.state.tags.forEach((tag) => {
+      if (!tag.hidden || tag.hidden == showHiddenTag) {
+        menuItems.push({ key: tag.id, label: tag.name });
+      }
+    })
+    menuItems.push({ itemIcon: <SwapOutlined />, key: 'switch' });
+    this.setState({ menuItems: menuItems, showHiddenTag: showHiddenTag });
+  }
+
   onMenuClick = (props: MenuInfo) => {
+    if (props.key == 'switch') {
+      this.renderMenu(!this.state.showHiddenTag);
+      return;
+    }
     for (let i = 0; i < this.state.tags.length; i++) {
       if (props.key === this.state.tags[i].id) {
         this.setState({ currentIndex: i });
