b62f5e95caa7ce205f1443b2b48df1ed95f7f679
呉真 <kuretru@gmail.com>
2023-03-30 14:01:29 +0800

    新增(Web)：Footer文字

--- Aries-Navigation-Web/src/components/Footer/index.tsx
+++ Aries-Navigation-Web/src/components/Footer/index.tsx
@@ -1,22 +1,22 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = '呉真出品';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
-      copyright={`${currentYear} ${defaultMessage}`}
+      copyright={`2019 - ${currentYear} ${defaultMessage}`}
       links={[
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/kuretru/Aries-Navigation',
+          blankTarget: true,
+        },
         {
           key: 'Ant Design Pro',
           title: 'Ant Design Pro',
           href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
-        {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
           blankTarget: true,
         },
         {
